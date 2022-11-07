c235a575ea5cc437f3ea7dd66df55d5e9b2ca9d9
Abigail LI <palmigloo@gmail.com>
2022-11-07 11:52:00 -0800

    complete react component

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+const { useState } = React;
+
+const GroceryList = (props) => (
+  <ul>
+    {props.itemList.map((groItem) => (
+      <GroceryListItem item={groItem} />
+    ))}
+  </ul>
+);
+
+const GroceryListItem = (props) => {
+  const [isDone, setIsDone] = useState(false);
+  const [hover, setHover] = useState(false);
+
+  const style = {
+    textDecoration: isDone ? 'line-through' : 'none',
+    fontWeight : hover ? 'bold' : ''
+  };
+
+  // const hoverEffect = () => (
+  //   setHover(true);
+  //   // e.target.style.fontWeight = 'bold';
+  // );
+
+  return (
+    <li style={style} onClick={() => setIsDone(!isDone)} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}>{props.item}
+    </li>
+  );
+};
+
+  ReactDOM.render(<GroceryList itemList={['coffee', 'tea']} />, document.getElementById('app'));
